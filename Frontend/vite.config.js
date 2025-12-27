2ebb3e87c6831cbd0eabab25b3e3bb565071e3a4
Akshith Reddy Kathulapalli <akshithreddy.bio@gmail.com>
2025-12-27 18:56:50 +0530

    updated vercel.json and vite config file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",              // ✅ correct and required for Vercel
+  base: "./",          
   server: {
     port: 5173,
   },
